d545cb1dad57cf0a448b353d134a7c2db8bc358a
106598 <106598@U-55184.domena.uksw.edu.pl>
2019-10-26 12:28:19 +0200

    Hello world

---
+++ nowy/src/nowy/klasa.java
@@ -0,0 +1,12 @@
+package nowy;
+
+public class klasa {
+	
+	public static void main(String[]args) {
+		
+		System.out.println("hello world!");
+		String hello= "hello world!";
+		System.out.print("napis'hello world' ma" + hello.length() + "znakow");
+	}
+
+}
