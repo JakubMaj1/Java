437d53ad113e65de1cd43e861331923a27b7fc68
JakubMaj1 <57034154+JakubMaj1@users.noreply.github.com>
2020-01-25 10:58:13 +0100

    Zadanie 2 25.01.20

---
+++ 25.01.2020/Zadanie2/src/JoinPractice.java
@@ -0,0 +1,10 @@
+
+
+public class JoinPractice extends Thread {
+    @Override
+    public void run() {
+        for(int i = 1;i <= 20;i++){
+                System.out.println(i);
+        }
+    }
+}
