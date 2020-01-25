0f564856eaf9e5b2775eb2945ee0eccefdcfb48a
JakubMaj1 <57034154+JakubMaj1@users.noreply.github.com>
2020-01-25 09:24:08 +0100

    Add files via upload

---
+++ 07.12.19/Zadanie 5/src/People.java
@@ -0,0 +1,16 @@
+//package Zadanie5;
+
+public class People implements Comparable {
+    public int wiek;
+    public String imie;
+
+    public People(int wiek , String imie){
+        this.wiek = wiek;
+        this.imie = imie;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
